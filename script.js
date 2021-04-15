e5cc670ad5dc570340c8650e2afa88eb81a45729
PaulB-H <p.bernardhall@gmail.com>
2021-04-15 02:09:24 -0400

    Added script to enable night mode
    Set this up to run by default, much easier on my eyes. Will build toggle switch in future

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 hljs.initHighlightingOnLoad();
 hljs.initLineNumbersOnLoad();
+
+const applyNightMode = () => {
+  document.querySelector("html").style.backgroundColor = "black";
+
+  document
+    .querySelectorAll("p, BLOCKQUOTE, h1, h2, h3, h4, h5")
+    .forEach((item) => {
+      item.style.color = "white";
+    });
+
+  document.querySelectorAll("code").forEach((item) => {
+    item.style.backgroundColor = "#555";
+    item.style.color = "white";
+  });
+
+  document.querySelectorAll(".notice").forEach((item) => {
+    item.style.color = "black";
+  });
+
+  document.querySelectorAll("a").forEach((item) => {
+    item.style.padding = "3px";
+    item.style.background = "#ccc";
+  });
+
+  document.querySelectorAll("hr").forEach((item) => {
+    item.style.color = "white";
+  });
+};
+
+applyNightMode();
