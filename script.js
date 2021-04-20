6ef27c2f46759dcb6d8e0870de770359f4c6a434
PaulB-H <p.bernardhall@gmail.com>
2021-04-19 23:58:44 -0400

    Moved some comments to notepad

--- script.js
+++ script.js
@@ -64,5 +64,37 @@ const revertNightMode = () => {
 const commandObject = {
   vscodecommands: [
     { command: "Ctrl + `", desc: "Show / hide integrated terminal" },
+    { command: "Ctrl + B", desc: "Open in browser (requires extension)" },
+    { command: "Ctrl + Shift + [", desc: "Fold (collapse) region" },
+    // { command: "Ctrl + B", desc: "Open in browser (requires extension)" },
+    // { command: "Ctrl + B", desc: "Open in browser (requires extension)" },
+    // { command: "Ctrl + B", desc: "Open in browser (requires extension)" },
   ],
 };
+
+console.log(commandObject);
+
+{
+  /* <pre><code class="md">Ctrl + Shift + [</code></pre>
+<blockquote>Fold (collapse) region</blockquote>
+
+<pre><code class="md">Ctrl + Shift + ]</code></pre>
+<blockquote>Unfold (uncollapse) region</blockquote>
+
+<p class="notice">
+  For the following commands, keep Ctrl held but press the other keys
+  one after another.
+</p>
+
+<pre><code class="md">Ctrl + K -> Ctrl + [</code></pre>
+<blockquote>Fold (collapse) all subregions</blockquote>
+
+<pre><code class="md">Ctrl + K -> Ctrl + ]</code></pre>
+<blockquote>Unfold (uncollapse) all subregions</blockquote>
+
+<pre><code class="md">Ctrl + K -> Ctrl + 0</code></pre>
+<blockquote>Fold (collapse) all regions</blockquote>
+
+<pre><code class="md">Ctrl + K -> Ctrl + J</code></pre>
+<blockquote>Unfold (uncollapse) all regions</blockquote> */
+}
